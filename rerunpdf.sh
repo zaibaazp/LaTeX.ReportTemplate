ad185fc45443679c53ee62cc4f72a499faa18d15
JOSE ALONSO SOLIS-LEMUS <Jose.Solis-Lemus@city.ac.uk>
2015-12-01 12:42:52 +0000

    Modified rerun script file.

--- rerunpdf.sh
+++ rerunpdf.sh
@@ -2,24 +2,60 @@
 # Delete everything except the .tex file, then runs pdflatex and bibtex the amount of
 # times necessary to produce a proper pdf file. 
 
+
+usage="
+$(basename "$0") [-h] [-d] -- program to compile your pdf file
+out of a LaTeX program. The syntax we use as default is the following:
+
+>  pdflatex -synctex=1 -interaction=nonstopmode filename
+
+Note that there is no '.tex' at the end of <filename>.
+Please don't use it.
+"
+    
 if [ -z $1 ]; then
-    echo "Usage (1):"
-    echo "./rerunpdf.sh <texfile>"
-    echo ""
+    echo "$usage"
     exit 1
 fi
+
+debugmode=0
+while getopts ':dh' option; do 
+    case "$option" in
+	h) echo "$usage"
+	    exit
+	    ;;
+	d) debugmode=1
+	    echo "DEBUG MODE ON"
+	    ;;
+	\?) echo "$usage" >&2
+	    exit 1
+	    ;;
+    esac
+done
+
+shift $((OPTIND-1))
 
 mv $1.tex ../
 rm $1.*
 mv ../$1.tex ./
 
-pdflatex -synctex=1 -interaction=nonstopmode $1
-
-# Uncomment both lines if you have a .bib file available in this directory.
-# bibtex $1
-# pdflatex -synctex=1 -interaction=nonstopmode $1
-pdflatex -synctex=1 -interaction=nonstopmode $1
+if [ "$debugmode" == "0" ]; then
+    pdflatex -synctex=1 -interaction=nonstopmode $1
+    # Uncomment both lines if you have a .bib file available in this directory.
+    bibtex $1
+    pdflatex -synctex=1 -interaction=nonstopmode $1
+    pdflatex -synctex=1 -interaction=nonstopmode $1
+else
+    pdflatex -synctex=1 $1
+    # Uncomment the two following lines if you have a .bib file 
+    #available in this directory.
+    bibtex $1
+    pdflatex -synctex=1 $1
+    pdflatex -synctex=1 $1
+fi
 
 clear
 
-echo Done!
+printf  "\nCompiling done!" 
+printf "\nIf you had a lot of errors, you can try the -d (debug) option.\n"
+
